d207f13d18123eed1dcf11b86c00ed6481b06b46
Dayanne Yuki Hatori <danaya16238@gmail.com>
2024-11-22 09:24:22 +0800

    Add files via upload

---
+++ Hatori_Activity01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double temperature;
+
+    // Input the temperature
+    cout << "Enter the temperature in Fahrenheit: ";
+    cin >> temperature;
+
+    // Conditions to determine which jacket to recommend
+    if (temperature < 32) {
+        cout << "Bring a heavy jacket." << endl;
+    } else if (temperature >= 32 && temperature <= 50) {
+        cout << "Bring a light jacket." << endl;
+    } else if (temperature > 50) {
+        cout << "No need to bring jacket." << endl;
+    }
+
+    return 0;
+}
