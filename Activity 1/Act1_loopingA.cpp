35e3d661cd9c1c1dcde999aec185443133d343fe
Dayanne Yuki Hatori <danaya16238@gmail.com>
2024-12-02 00:41:43 +0800

    Add files via upload

--- Activity 1/Act1_loopingA.cpp
+++ Activity 1/Act1_loopingA.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+	for (int i = 01; i < 9; ++i) {
+		for (int j = 1; j < 8; ++j) {
+			if ((i + j) % 2 == 0) {
+				cout << "# ";
+			} else {
+				cout << " * ";
+			}
+		}
+		cout << endl;
+	}
+}
