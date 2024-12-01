91e10f9889b241e7fc14524d7fd81cd6fea4d9d1
Dayanne Yuki Hatori <danaya16238@gmail.com>
2024-12-02 00:49:26 +0800

    Add files via upload

---
+++ Activity 2/Act2_loopingI.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	for(int i = 0; i < 6; i++) {
+		for(int j = 0; j < 10; j++) 
+		{
+			cout << " ";
+		}
+		for(int j = 0; j < i; j++) 
+		{
+			cout << "^";
+		}
+		for(int j = 0; j < 1; j++)
+			{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	for(int i = 0; i < 2; i++) 
+	{
+		for (int j = 0; j < 10; j++)
+			{
+			cout << "#";
+		}
+		for(int j = 0; j < 6; j++)
+			{
+			cout << "^";
+		}
+		for (int j = 0; j < 1; j++)
+			{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	
+	for(int i = 0; i < 6; i++)
+		{
+		for(int j = 0; j < 10; j++)
+			{
+			cout << " ";
+		}
+		for(int j = i; j < 5; j++)
+			{
+			cout << "^";
+		}
+		for(int j = 0; j < 1; j++) 
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+}
